cc4c00eeee9d9cf6831cec40223b6e471dd437b2
Ian2304 <iankypr123@gmail.com>
2023-11-26 21:22:54 -0500

    Se agrega código js y json para tienda virtual

---
+++ Ecommerce-website/js/carrito.js
@@ -0,0 +1,175 @@
+let productosEnCarrito = localStorage.getItem("productos-en-carrito");
+productosEnCarrito = JSON.parse(productosEnCarrito);
+const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
+const contenedorCarritoProductos = document.querySelector("#carrito-productos");
+const contenedorCarritoAcciones = document.querySelector("#carrito-acciones");
+const contenedorCarritoComprado = document.querySelector("#carrito-comprado");
+let botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
+const botonVaciar = document.querySelector("#carrito-acciones-vaciar");
+const contenedorTotal = document.querySelector("#total");
+const botonComprar = document.querySelector("#carrito-acciones-comprar");
+
+
+function cargarProductosCarrito() {
+    if (productosEnCarrito && productosEnCarrito.length > 0) {
+
+        contenedorCarritoVacio.classList.add("disabled");
+        contenedorCarritoProductos.classList.remove("disabled");
+        contenedorCarritoAcciones.classList.remove("disabled");
+        contenedorCarritoComprado.classList.add("disabled");
+    
+        contenedorCarritoProductos.innerHTML = "";
+    
+        productosEnCarrito.forEach(producto => {
+    
+            const div = document.createElement("div");
+            div.classList.add("carrito-producto");
+            div.innerHTML = `
+                <img class="carrito-producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+                <div class="carrito-producto-titulo">
+                    <small>Título</small>
+                    <h3>${producto.titulo}</h3>
+                </div>
+                <div class="carrito-producto-cantidad">
+                    <small>Cantidad</small>
+                    <p>${producto.cantidad}</p>
+                </div>
+                <div class="carrito-producto-precio">
+                    <small>Precio</small>
+                    <p>$${Number(producto.precio).toFixed(3)}</p>
+
+                    </div>
+                <div class="carrito-producto-subtotal">
+                    <small>Subtotal</small>
+                    <p>$${(Number(producto.precio) * producto.cantidad).toFixed(3)}</p>
+                    </div>
+                <button class="carrito-producto-eliminar" id="${producto.id}"><i class="bi bi-trash-fill"></i></button>
+            `;
+    
+            contenedorCarritoProductos.append(div);
+        })
+    actualizarBotonesEliminar();
+    actualizarTotal();
+	
+    } 
+    
+    else {
+        contenedorCarritoVacio.classList.remove("disabled");
+        contenedorCarritoProductos.classList.add("disabled");
+        contenedorCarritoAcciones.classList.add("disabled");
+        contenedorCarritoComprado.classList.add("disabled");
+    }
+
+}
+
+cargarProductosCarrito();
+
+function actualizarBotonesEliminar() {
+    botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
+
+    botonesEliminar.forEach(boton => {
+        boton.addEventListener("click", eliminarDelCarrito);
+    });
+}
+
+function eliminarDelCarrito(e) {
+    Toastify({
+        text: "Producto eliminado",
+        duration: 3000,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #4b33a8, #785ce9)",
+          borderRadius: "2rem",
+          textTransform: "uppercase",
+          fontSize: ".75rem"
+        },
+        offset: {
+            x: '1.5rem', // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+            y: '1.5rem' // vertical axis - can be a number or a string indicating unity. eg: '2em'
+          },
+        onClick: function(){} // Callback after click
+      }).showToast();
+
+    const idBoton = e.currentTarget.id;
+    const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+    
+    productosEnCarrito.splice(index, 1);
+    cargarProductosCarrito();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+
+}
+
+botonVaciar.addEventListener("click", vaciarCarrito);
+function vaciarCarrito() {
+
+    Swal.fire({
+        title: '¿Estás seguro?',
+        icon: 'question',
+        html: `Se van a borrar ${productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0)} productos.`,
+        showCancelButton: true,
+        focusConfirm: false,
+        confirmButtonText: 'Sí',
+        cancelButtonText: 'No'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            productosEnCarrito.length = 0;
+            localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+            cargarProductosCarrito();
+        }
+      })
+}
+
+/*
+function actualizarTotal() {
+    const totalCalculado = productosEnCarrito.reduce((acc, producto) => acc + (Number(producto.precio) * producto.cantidad), 0);
+    total.innerText = `$${totalCalculado.toFixed(3)}`;
+}
+*/
+
+function actualizarTotal() {
+    const totalCalculado = productosEnCarrito.reduce((acc, producto) => acc + (Number(producto.precio) * producto.cantidad), 0);
+    const totalFormateado = new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(totalCalculado);
+    total.innerText= totalFormateado;
+    
+}
+
+
+
+
+
+botonComprar.addEventListener("click", comprarCarrito);
+function comprarCarrito() {
+
+    productosEnCarrito.length = 0;
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+    
+    contenedorCarritoVacio.classList.add("disabled");
+    contenedorCarritoProductos.classList.add("disabled");
+    contenedorCarritoAcciones.classList.add("disabled");
+    contenedorCarritoComprado.classList.remove("disabled");
+
+}
+
+function agregarAlCarrito(e) {
+    // ... (código existente)
+
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
+
+    if(productosEnCarrito.some(producto => producto.id === idBoton)) {
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
+    } else {
+        productoAgregado.cantidad = 1;
+        productoAgregado.precio = Number(productoAgregado.precio); // Asegúrate de convertir el precio a número
+        productosEnCarrito.push(productoAgregado);
+    }
+
+    actualizarNumerito();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+}
---
+++ Ecommerce-website/js/main.js
@@ -0,0 +1,588 @@
+const productos = [
+
+    //REPUESTOS
+    {
+        id: "hardware-01",
+        titulo: "CABLE RJ45",
+        imagen: "./img/hardware/01.jpg",
+        categoria: {
+            nombre: "hardware",
+            id: "hardware"
+        },
+        precio: "7.000"
+    },
+    {
+        id:"hardware-02",
+        titulo:"CABLE HDMI",
+        imagen:"./img/hardware/02.jpg",
+        categoria: {
+            nombre:"hardware",
+            id: "hardware"
+        },
+        precio: "15.000"
+
+    },
+    {
+        id:"hardware-03",
+        titulo:"CARGADOR ASUS DE 19.5V X 6.15A",
+        imagen:"./img/hardware/03.jpg",
+        categoria: {
+            nombre:"hardware",
+            id: "hardware"
+        },
+        precio: "42.000"
+
+    },
+    {
+        id:"hardware-05",
+        titulo:"TECLADO GENIUS",
+        imagen:"./img/hardware/05.jpg",
+        categoria: {
+            nombre:"hardware",
+            id: "hardware"
+        },
+        precio: "18.000"
+
+    },
+    {
+        id:"hardware-06",
+        titulo:"HDD 500Gb",
+        imagen:"./img/hardware/06.jpg",
+        categoria: {
+            nombre:"hardware",
+            id: "hardware"
+        },
+        precio: "120.000"
+
+    },
+    {
+        id:"hardware-07",
+        titulo:"FUENTE DE ALIMENTACIÓN PARA PC ATX",
+        imagen:"./img/hardware/07.jpg",
+        categoria: {
+            nombre:"hardware",
+            id: "hardware"
+        },
+        precio: "60.000"
+
+    },
+    {
+        id:"hardware-08",
+        titulo:"ALMACENAMIENTO USB 16GB",
+        imagen:"./img/hardware/08.jpg",
+        categoria: {
+            nombre:"hardware",
+            id: "hardware"
+        },
+        precio: "12.000"
+
+    },
+    {
+        id:"hardware-09",
+        titulo:" D'Link DSL-124 Wireless N300 ADSL2",
+        imagen:"./img/hardware/09.jpg",
+        categoria:{
+            nombre:"hardware",
+            id:"hardware",
+        },
+        precio: "150.000"
+    },
+    {
+        id: "hardware-10",
+        titulo: "CABLE USB X USB",
+        imagen: "./img/hardware/10.jpg",
+        categoria: {
+            nombre: "hardware",
+            id: "hardware"
+        },
+        precio: "12.000"
+    },
+    {
+        id: "hardware-11",
+        titulo: "CABLE JACK MACHO X MACHO",
+        imagen: "./img/hardware/11.jpg",
+        categoria: {
+            nombre: "hardware",
+            id: "hardware"
+        },
+        precio: "8.000"
+    },
+    {
+        id: "hardware-12",
+        titulo: "CABLE Rca 3x3",
+        imagen: "./img/hardware/12.jpg",
+        categoria: {
+            nombre: "hardware",
+            id: "hardware"
+        },
+        precio: "6.000"
+    },
+    {
+        id: "hardware-13",
+        titulo: "CABLE Rj11",
+        imagen: "./img/hardware/13.jpg",
+        categoria: {
+            nombre: "hardware",
+            id: "hardware"
+        },
+        precio: "7.000"
+    },
+    {
+        id: "hardware-14",
+        titulo: "D-LINK DSL-30CF SPLITTER ",
+        imagen: "./img/hardware/14.jpg",
+        categoria: {
+            nombre: "hardware",
+            id: "hardware"
+        },
+        precio: "2.000"
+    },
+    {
+        id: "hardware-15",
+        titulo: "CARGADOR HP 1000 19.5V X 3.33A",
+        imagen: "./img/hardware/15.jpg",
+        categoria: {
+            nombre: "hardware",
+            id: "hardware"
+        },
+        precio: "27.000"
+    },
+    {
+        id: "hardware-16",
+        titulo: "CABLE DIV X DIV",
+        imagen: "./img/hardware/16.jpg",
+        categoria: {
+            nombre: "hardware",
+            id: "hardware"
+        },
+        precio: "14.000"
+    },
+    {
+        id: "hardware-17",
+        titulo: "ADAPTADOR DVI X VGA",
+        imagen: "./img/hardware/17.jpg",
+        categoria: {
+            nombre: "hardware",
+            id: "hardware"
+        },
+        precio: "3.000"
+    },
+    {
+        id: "hardware-18",
+        titulo: "CABLE VGA",
+        imagen: "./img/hardware/18.jpg",
+        categoria: {
+            nombre: "hardware",
+            id: "hardware"
+        },
+        precio: "6.000"
+    },
+    {
+        id: "hardware-19",
+        titulo: "CABLE DE PODER PS4",
+        imagen: "./img/hardware/19.jpg",
+        categoria: {
+            nombre: "hardware",
+            id: "hardware"
+        },
+        precio: "6.000"
+    },
+    {
+        id: "hardware-20",
+        titulo: "CABLE DE PODER",
+        imagen: "./img/hardware/20.jpg",
+        categoria: {
+            nombre: "hardware",
+            id: "hardware"
+        },
+        precio: "6.000"
+    },{
+        id: "hardware-21",
+        titulo: "Direct Tv cargador L14 12v 1.5 A",
+        imagen: "./img/hardware/21.jpg",
+        categoria: {
+            nombre: "hardware",
+            id: "hardware"
+        },
+        precio: "34.000"
+    
+    },
+    {
+        id: "hardware-22",
+        titulo: "ADAPTADOR SAMGUNG A1514_DSM",
+        imagen: "./img/hardware/22.jpg",
+        categoria: {
+            nombre: "hardware",
+            id: "hardware"
+        },
+        precio: "53.000"
+    },{
+        id: "hardware-23",
+        titulo: "ADAPTADOR EPSON M235A",
+        imagen: "./img/hardware/23.jpg",
+        categoria: {
+            nombre: "hardware",
+            id: "hardware"
+        },
+        precio: "45.000"
+    },
+    {
+        id: "hardware-24",
+        titulo: "CABLE SATA",
+        imagen: "./img/hardware/24.jpg",
+        categoria: {
+            nombre: "hardware",
+            id: "hardware"
+        },
+        precio: "5.000"
+    },
+    //CELULARES
+    {
+        id: "celular-01",
+        titulo: "SAMSUNG GALAXY S7 EDGE",
+        imagen: "./img/celulares/01.jpg",
+        categoria: {
+            nombre: "celulares",
+            id: "celulares"
+        },
+        precio: "250.000"
+    },
+    {
+        id: "celular-02",
+        titulo: "SAMSUMG GALAXY J3 2016",
+        imagen: "./img/celulares/02.jpg",
+        categoria: {
+            nombre: "celulares",
+            id: "celulares"
+        },
+        precio: "70.000"
+    },
+    {
+        id:"celular-03",
+        titulo:"LG T-395",
+        imagen:"./img/celulares/03.jpg",
+        categoria: {
+            nombre:"celulares",
+            id: "celulares"
+        },
+        precio: "25.000"
+
+    },
+        {
+        id:"celular-04",
+        titulo:"MOTOROLA G52",
+        imagen:"./img/celulares/04.jpg",
+        categoria: {
+            nombre:"celulares",
+            id: "celulares"
+        },
+        precio: "520.000"
+
+    },
+        {
+        id:"celular-05",
+        titulo:"NOKIA G21",
+        imagen:"./img/celulares/05.jpg",
+        categoria: {
+            nombre:"celulares",
+            id: "celulares"
+        },
+        precio: "800.000"
+
+    },
+    {
+        id:"celular-06",
+        titulo:"SAMSUNG GALAXY M13",
+        imagen:"./img/celulares/06.jpg",
+        categoria: {
+            nombre:"celulares",
+            id: "celulares"
+        },
+        precio: "560.000"
+
+    },
+    {
+        id:"celular-07",
+        titulo:"KALLEY-SILVER-MAX PRO-2",
+        imagen:"./img/celulares/07.jpg",
+        categoria: {
+            nombre:"celulares",
+            id: "celulares"
+        },
+        precio: "310.000"
+
+    },
+    {
+        id:"celular-08",
+        titulo:"NOKIA G50",
+        imagen:"./img/celulares/08.jpg",
+        categoria: {
+            nombre:"celulares",
+            id: "celulares"
+        },
+        precio: "855.000"
+
+    },
+    
+
+    //COMPUTADORES 
+    {
+        id:"computador-01",
+        titulo:"PORTÁTIL ASUS X515",
+        imagen:"./img/computadores/01.jpg",
+        categoria: {
+            nombre:"computadores",
+            id: "computadores"
+        },
+        precio: "940.000"
+
+    },
+    {
+        id:"computador-02",
+        titulo:"ASUS VIVOBOOK GO 15",
+        imagen:"./img/computadores/02.jpg",
+        categoria: {
+            nombre:"computadores",
+            id: "computadores"
+        },
+        precio: "750.000"
+
+    },
+
+    {
+        id:"computador-03",
+        titulo:"ASUS TUF GAMING F15",
+        imagen:"./img/computadores/03.jpg",
+        categoria: {
+            nombre:"computadores",
+            id: "computadores"
+        },
+        precio: "950.000"
+
+    },
+
+    {
+        id:"computador-04",
+        titulo:"LENOVO IDEAPAD GAMING 3",
+        imagen:"./img/computadores/04.jpg",
+        categoria: {
+            nombre:"computadores",
+            id: "computadores"
+        },
+        precio: "800.000"
+
+    },
+
+    {
+        id:"computador-05",
+        titulo:"HP GAMING VICTUS",
+        imagen:"./img/computadores/05.jpg",
+        categoria: {
+            nombre:"computadores",
+            id: "computadores"
+        },
+        precio: "757.000"
+
+    },
+
+    {
+        id:"computador-06",
+        titulo:"LENOVO P11 2° GENERACION-11-5",
+        imagen:"./img/computadores/06.jpg",
+        categoria: {
+            nombre:"computadores",
+            id: "computadores"
+        },
+        precio: "960.000"
+
+    },
+    {
+        id:"computador-07",
+        titulo:"DELL VOSTRO 3400",
+        imagen:"./img/computadores/07.jpg",
+        categoria: {
+            nombre:"computadores",
+            id: "computadores"
+        },
+        precio: "800.000"
+
+    },
+
+
+]
+/*
+fetch("./js/productos.js")
+    .then(response => response.json())
+    .then(data => {
+        productos = data;
+        cargarProductos(productos);
+    })
+*/
+
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar = document.querySelectorAll(".producto-agregar");
+const numerito = document.querySelector("#numerito");
+
+
+botonesCategorias.forEach(boton => boton.addEventListener("click", () => {
+    aside.classList.remove("aside-visible");
+}))
+
+
+
+function cargarProductos(productosElegidos) {
+
+    contenedorProductos.innerHTML = "";
+
+    productosElegidos.forEach(producto => {
+
+        const div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `
+            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <div class="producto-detalles">
+                <h3 class="producto-titulo">${producto.titulo}</h3>
+                <p class="producto-precio">$${producto.precio}</p>
+                <button class="producto-agregar" id="${producto.id}">Agregar</button>
+            </div>
+        `;
+
+        contenedorProductos.append(div);
+    })
+
+    actualizarBotonesAgregar();
+}
+
+//a ver
+
+
+
+botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+
+        botonesCategorias.forEach(boton => boton.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+
+        if (e.currentTarget.id != "todos") {
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosBoton);
+        } else {
+            tituloPrincipal.innerText = "Todos los productos";
+            cargarProductos(productos);
+        }
+
+    })
+});
+
+function actualizarBotonesAgregar() {
+    botonesAgregar = document.querySelectorAll(".producto-agregar");
+
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+    });
+}
+
+
+
+let productosEnCarrito;
+
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+
+if (productosEnCarritoLS) {
+    productosEnCarrito = JSON.parse(productosEnCarritoLS);
+    actualizarNumerito();
+} else {
+    productosEnCarrito = [];
+}
+
+function agregarAlCarrito(e) {
+
+    Toastify({
+        text: "Producto agregado",
+        duration: 3000,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #4b33a8, #785ce9)",
+          borderRadius: "2rem",
+          textTransform: "uppercase",
+          fontSize: ".75rem"
+        },
+        offset: {
+            x: '1.5rem', // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+            y: '1.5rem' // vertical axis - can be a number or a string indicating unity. eg: '2em'
+          },
+        onClick: function(){} // Callback after click
+      }).showToast();
+
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
+
+    if(productosEnCarrito.some(producto => producto.id === idBoton)) {
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
+    } else {
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
+    }
+
+    actualizarNumerito();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+}
+
+function actualizarNumerito() {
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito;
+}
+
+//SCROLL REVEAL
+/*
+window.sr = ScrollReveal();
+
+    ScrollReveal().reveal('.titulo-TB', 
+    { delay: 500,
+      origin:'left',
+      distance:'-100px',
+    });
+     
+    ScrollReveal().reveal('.texto__em', 
+    { delay: 900,
+      origin:'top',
+      distance:'-100px',
+    }); 
+
+    ScrollReveal().reveal('.boton-carrito', 
+    { delay: 600,
+      origin:'top',
+      distance:'-100px',
+    }); 
+    ScrollReveal().reveal('#computadores', 
+    { delay: 700,
+      origin:'top',
+      distance:'-100px',
+    }); 
+    ScrollReveal().reveal('#hardware', 
+    { delay: 800,
+      origin:'top',
+      distance:'-100px',
+    }); 
+     ScrollReveal().reveal('#celulares', 
+    { delay: 900,
+      origin:'top',
+      distance:'-100px',
+    });
+    ScrollReveal().reveal('.boton-menu.active', 
+    { delay: 1000,
+      origin:'top',
+      distance:'-100px',
+    }); 
+     
+    
+   */
